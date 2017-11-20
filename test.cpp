0a544f71ea21573a7efdd888db20ce06fd232cf0
niupple <niupple@gmail.com>
2017-11-20 23:36:12 +0800

    Hello World

---
+++ test.cpp
@@ -0,0 +1,32 @@
+/* Copyright dyj (c) 2017
+ * any codes cannot be used for business
+ * Problem:
+ * BUAA开学季!
+ * 高举中国特色社会主义伟大旗帜，为实现中华民族伟大复兴减少BUG!
+ * Uprise the flag of Socialism with Chinese characterastics, to reduce bugs for the great revision of the Chinese people.
+ * */
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <climits>
+#include <cstring>
+#define NAME "test"
+#define read(x) scanf("%d", &x)
+#ifdef DEBUG
+#	define DP(fmt, ...) fprintf(stderr, fmt, __VA_ARGS__)
+#else
+#	define DP(fmt, ...) 0
+#endif
+
+using namespace std;
+
+int main()
+{
+	char a[3], b[] = "China";
+	//a = b;
+	printf("%s\n", a);
+}
+
